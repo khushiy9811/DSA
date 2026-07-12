943e8609ed92bcf315921edd996ae4f1e046f156
khushiy9811 <151054071+khushiy9811@users.noreply.github.com>
2026-07-13 01:11:29 +0530

    Day 4

--- Two Pointer/Reverse Vowels of a String.cpp
+++ Two Pointer/Reverse Vowels of a String.cpp
@@ -1,1 +1,28 @@
+class Solution {
+public:
+    bool isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+               c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
+    }
 
+    string reverseVowels(string s) {
+        int start = 0;
+        int end = s.size() - 1;
+
+        while (start < end) {
+            if (!isVowel(s[start])) {
+                start++;
+            }
+            else if (!isVowel(s[end])) {
+                end--;
+            }
+            else {
+                swap(s[start], s[end]);
+                start++;
+                end--;
+            }
+        }
+
+        return s;
+    }
+};
